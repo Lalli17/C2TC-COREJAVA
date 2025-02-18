16c774c0f1d171b50270e747e8ca2120c4704ed1
ShreeCP <lalitha.shreecp@harman.com>
2025-02-18 17:27:10 +0530

    sample file

---
+++ sample/sample.js
@@ -0,0 +1,11 @@
+function countToN(n) {
+    let count = 1;
+    while (true) {
+       if (count > n) {
+          return;
+       }
+       console.log(count);
+       count++;
+    }
+ }
+ countToN(10);
